cbca47230f480d35688675da105efc6aa64be05b
Ron Gayowsky <ron.gayowsky@gmail.com>
2022-09-17 19:17:14 -0400

    Fix test, now twosum reports as correct

--- c++/src/leetcode/001-twosum/twosum.cpp
+++ c++/src/leetcode/001-twosum/twosum.cpp
@@ -7,7 +7,6 @@ std::vector<int> TwoSum::twoSum(std::vector<int> &nums, int target) {
     std::unordered_map<int, int> x;
     std::vector<int> ans;
     for (int i = 0; i < nums.size(); ++i) {
-      std::cout << nums[i] << ", ";
       int z = target - nums[i];
       if (x.find(z) != x.end()) {
         ans.push_back(i);
@@ -15,11 +14,6 @@ std::vector<int> TwoSum::twoSum(std::vector<int> &nums, int target) {
       } else {
         x[nums[i]] = i;
       }
-    }
-    std::cout << "\n";
-
-    for (int i = 0; i < ans.size(); ++i) {
-      std::cout << ans[i] << ", ";
     }
 
     return ans;
--- c++/test/leetcode/001-twosum/test_twosum.cpp
+++ c++/test/leetcode/001-twosum/test_twosum.cpp
@@ -7,7 +7,5 @@ TEST(TwoSumTests, ExampleInput) {
     std::vector<int> nums{2,7,11,15};
     int target = 9;
     std::vector<int> answer = TwoSum::twoSum(nums, target);
-    EXPECT_EQ(2, answer[0]);
-    EXPECT_EQ(7, answer[1]);
-    EXPECT_EQ(target, answer[0] + answer[1]);
+    EXPECT_EQ(target, nums[answer[0]] + nums[answer[1]]);
 }
